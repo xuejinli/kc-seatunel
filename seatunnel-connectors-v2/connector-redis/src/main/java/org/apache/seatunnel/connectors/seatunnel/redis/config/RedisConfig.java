6f3eb09b436725321dcc7fcfb849a1ac1c26b6c6
limin <limin@wedobest.com.cn>
2024-10-29 19:42:04 +0800

    [Improve][Connector-Redis] Redis support custom key and value

--- seatunnel-connectors-v2/connector-redis/src/main/java/org/apache/seatunnel/connectors/seatunnel/redis/config/RedisConfig.java
+++ seatunnel-connectors-v2/connector-redis/src/main/java/org/apache/seatunnel/connectors/seatunnel/redis/config/RedisConfig.java
@@ -131,7 +131,7 @@ public class RedisConfig {
             Options.key("support_custom_key")
                     .booleanType()
                     .defaultValue(false)
-                    .withDescription("the key can be customized by the field value in the upstream data.");
+                    .withDescription("if true, the key can be customized by the field value in the upstream data.");
 
     public static final Option<String> VALUE_FIELD =
             Options.key("value_field")
